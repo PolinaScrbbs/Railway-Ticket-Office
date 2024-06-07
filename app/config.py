6bebf31e68b5ad40a682354511dd8821acdb7a94
PolinaScrbbs <polinascaraboobs@gmail.com>
2024-06-07 10:09:35 +0300

    Багофикс
    Удалил асинхронные пакеты с requirements
    
    Добавил очистку переменных виртуального окружения

--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
 
+os.environ.pop('DB_HOST', None)
+os.environ.pop('DB_PORT', None)
+os.environ.pop('DB_NAME', None)
+os.environ.pop('DB_USER', None)
+os.environ.pop('DB_PASS', None)
+
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
@@ -8,3 +14,5 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+
+print(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS)
